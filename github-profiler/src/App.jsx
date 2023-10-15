5ffe07137770393c0373df0e1c1353b34288bec7
Shivam Yadav <shivamyadav93000@gmail.com>
2023-10-15 17:57:23 +0530

    done

--- github-profiler/src/App.jsx
+++ github-profiler/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import HomePage from "./component/homepage";
+import Profile from "./component/profile";
+import RepoDetail from "./component/reposDetail";
+import Following from "./component/following";
+import Followers from "./component/followers";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "profile/:id",
+    element: <Profile />,
+  },
+  {
+    path: "/reposDetail/:id/:Detail",
+    element: <RepoDetail />,
+  },
+  {
+    path: "/following/:id",
+    element: <Following />,
+  },
+  {
+    path: "/followers/:id",
+    element: <Followers />,
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
